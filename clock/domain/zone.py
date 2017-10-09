ea9c197aaa25bb92e1393125bb03766c40f5e605
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-09 18:07:15 +0200

    Split Zone and ZoneFormat

--- clock/domain/zone.py
+++ clock/domain/zone.py
@@ -18,22 +18,42 @@ class Zone:
         """
         return self.zone_name
 
-    @property
-    def zone(self):
+    @staticmethod
+    def utc():
+        """Returns the UTC zone"""
+        return utc
+
+
+utc = Zone("UTC")
+
+
+class ZoneFormat:
+    @staticmethod
+    def zone_name(zone: Zone):
         """
-        Returns the zone name as stored by the timezone object (it should be equal to self.zone_name).
+        Returns the zone name passed to the zone constructor.
 
         Example: 'Europe/Madrid'
         """
-        return self.timezone.zone
+        return zone.zone_name
 
-    def __str__(self):
+    @staticmethod
+    def zone(zone: Zone):
         """
-        Returns the timezone string representation, which should be equal to self.zone.
+        Returns the zone name as stored by the timezone object (it should be equal to zone.zone_name).
 
         Example: 'Europe/Madrid'
         """
-        return self.timezone.__str__()
+        return zone.timezone.zone
+
+    @staticmethod
+    def zone_str(zone: Zone):
+        """
+        Returns the timezone string representation, which should be equal to the result of self.zone.
+
+        Example: 'Europe/Madrid'
+        """
+        return zone.timezone.__str__()
 
     @staticmethod
     def tzname(date_time: datetime.datetime):
@@ -51,33 +71,37 @@ class Zone:
         Uses ``babel.dates.get_timezone_gmt`` to get the GMT offset of the date_time.
         See its doc for more info.
 
-        Example: 'GMT-07:00', '-07' (short)
+        Example: 'GMT+02:00', '+0200' (short)
         """
         return babel.dates.get_timezone_gmt(date_time, locale=locale, width="short" if short else "long", return_z=return_z)
 
-    def location(self, locale: Locale):
+    @staticmethod
+    def location(zone: Zone, locale: Locale):
         """
         Returns the full location string of the timezone as provided by ``babel``.
 
         Example: 'hora de España (Madrid)'
         """
-        return babel.dates.get_timezone_location(self.timezone, locale=locale)
+        return babel.dates.get_timezone_location(zone.timezone, locale=locale)
 
-    def location_city(self, locale: Locale):
+    @staticmethod
+    def location_city(zone: Zone, locale: Locale):
         """
         Returns the main city for the timezone, provided by ``babel``.
 
         Example: 'Madrid'
         """
-        return babel.dates.get_timezone_location(self.timezone, locale=locale, return_city=True)
+        return babel.dates.get_timezone_location(zone.timezone, locale=locale, return_city=True)
 
-    def name(self, locale: Locale, short=False, zone_variant=None):
+    @staticmethod
+    def name(zone: Zone, locale: Locale, short=False, zone_variant=None):
         """
-        Returns the default timezone name as provided by ``babel``, with no daylight saving consideration.
+        Returns the default timezone name as provided by ``babel``.
+        Daylight saving is not taken into account, unless you specify a ``zone_variant``.
 
         Example: 'hora de Europa central', 'CET' (short)
         """
-        return babel.dates.get_timezone_name(self.timezone, locale=locale, width="short" if short else "long", zone_variant=zone_variant)
+        return babel.dates.get_timezone_name(zone.timezone, locale=locale, width="short" if short else "long", zone_variant=zone_variant)
 
     @staticmethod
     def name_at_a_date_time(date_time: datetime.datetime, locale: Locale, short=False):
@@ -89,16 +113,9 @@ class Zone:
         """
         return babel.dates.get_timezone_name(date_time, locale=locale, width="short" if short else "long")
 
-    def name_zone(self, locale: Locale):
+    @staticmethod
+    def name_zone(zone: Zone, locale: Locale):
         """
         Returns the zone name from ``babel``. Should match self.zone_name and self.zone.
         """
-        return babel.dates.get_timezone_name(self.timezone, locale=locale, return_zone=True)
-
-    @staticmethod
-    def utc():
-        """Returns the UTC zone"""
-        return utc
-
-
-utc = Zone("UTC")
+        return babel.dates.get_timezone_name(zone.timezone, locale=locale, return_zone=True)
