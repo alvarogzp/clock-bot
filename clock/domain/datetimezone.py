bbbadb34f35d2fcf433cc30ce57829e0922f8347
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-10 15:12:53 +0200

    Add more formatting options in DateTimeZoneFormatter

--- clock/domain/datetimezone.py
+++ clock/domain/datetimezone.py
@@ -23,11 +23,29 @@ class DateTimeZoneFormatter:
     def id(self):
         return self.date_time_zone.id()
 
-    def datetime(self):
-        return babel.dates.format_datetime(self.date_time_zone.date_time, locale=self.locale)
+    def date(self, format="medium"):
+        return babel.dates.format_date(self.date_time_zone.date_time, locale=self.locale, format=format)
 
-    def timezone(self):
+    def time(self, format="medium"):
+        return babel.dates.format_time(self.date_time_zone.date_time, locale=self.locale, format=format)
+
+    def datetime(self, format="medium"):
+        return babel.dates.format_datetime(self.date_time_zone.date_time, locale=self.locale, format=format)
+
+    def timezone_location(self):
         return self.__title(ZoneFormatter.location(self.date_time_zone.zone, self.locale))
+
+    def timezone_zone(self):
+        return ZoneFormatter.zone_name(self.date_time_zone.zone)
+
+    def timezone_tzname(self):
+        return ZoneFormatter.tzname(self.date_time_zone.date_time)
+
+    def timezone_name(self):
+        return self.__title(ZoneFormatter.name_at_a_date_time(self.date_time_zone.date_time, self.locale))
+
+    def timezone_offset(self):
+        return ZoneFormatter.gmt_offset(self.date_time_zone.date_time, self.locale)
 
     @staticmethod
     def __title(text):
