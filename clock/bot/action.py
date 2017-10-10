e9e1e1fb1cd166bd4802ab8b697efb980e6be2cc
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-10 12:55:59 +0200

    Add pagination support

--- clock/bot/action.py
+++ clock/bot/action.py
@@ -6,6 +6,9 @@ from clock.domain.time import TimePoint
 from clock.domain.finder import ZoneFinder
 
 
+MAX_RESULTS_PER_QUERY = 50
+
+
 class InlineClockAction(Action):
     def process(self, event):
         query_id = event.query.id
@@ -16,6 +19,12 @@ class InlineClockAction(Action):
 
         zones = ZoneFinder.find(event.query.query, locale)
 
+        offset = self.__get_offset(event)
+        offset_end = offset + MAX_RESULTS_PER_QUERY
+        next_offset = self.__get_next_offset(len(zones), offset_end)
+
+        zones = zones[offset:offset_end]
+
         results = []
         for zone in zones:
             date_time_zone = DateTimeZone(current_time, zone)
@@ -23,12 +32,31 @@ class InlineClockAction(Action):
             inline_date_time_zone_result_formatter = InlineResultFormatter(date_time_zone_formatter)
             results.append(inline_date_time_zone_result_formatter.result())
 
-        self.api.answerInlineQuery(inline_query_id=query_id, results=results, cache_time=0, is_personal=True)
+        self.api.answerInlineQuery(
+            inline_query_id=query_id,
+            results=results,
+            next_offset=next_offset,
+            cache_time=0,
+            is_personal=True
+        )
 
     @staticmethod
     def __get_locale(event):
         user_locale_code = event.query.from_.language_code
         return Locale.parse(user_locale_code, sep="-")
+
+    @staticmethod
+    def __get_offset(event):
+        offset = event.query.offset
+        if offset and offset.isdigit():
+            return int(offset)
+        return 0
+
+    @staticmethod
+    def __get_next_offset(result_number, offset_end):
+        if result_number > offset_end:
+            return str(offset_end)
+        return None
 
 
 class InlineResultFormatter:
