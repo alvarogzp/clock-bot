577688019e649677c229ceec5ff147707190b06c
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-11 19:53:34 +0200

    Add cache to TimePoint for its at() function, as it is called many times repeatedly in finder and then on the results

--- clock/domain/time.py
+++ clock/domain/time.py
@@ -4,6 +4,7 @@ import pytz
 import time
 
 from clock.domain.zone import Zone
+from clock.util import Cache
 
 
 class TimePoint:
@@ -11,12 +12,16 @@ class TimePoint:
         self.timestamp = timestamp
         # this is the most direct way to get a UTC aware datetime from a timestamp
         self.utc = pytz.utc.localize(datetime.datetime.utcfromtimestamp(timestamp))
+        self._at_cache = Cache()
 
     def id(self):
         return str(self.timestamp)
 
     def at(self, zone: Zone):
         """:rtype: datetime.datetime"""
+        return self._at_cache.get_or_generate(zone.zone_name, lambda: self._at__no_cache(zone))
+
+    def _at__no_cache(self, zone: Zone):
         return self.utc.astimezone(zone.timezone)
 
     @staticmethod
