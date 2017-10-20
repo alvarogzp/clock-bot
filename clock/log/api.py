e7884020654c772791e9088d3ae5391cf93ef231
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-20 19:16:11 +0200

    Add log support for queries and chosen results

---
+++ clock/log/api.py
@@ -0,0 +1,61 @@
+from babel import Locale
+from bot.action.util.textformat import FormattedText
+from bot.api.domain import ApiObject
+from bot.logger.logger import Logger
+
+from clock.domain.time import TimePoint
+from clock.log.formatter import LogFormatter
+
+
+LOG_TAG_QUERY = FormattedText().normal("QUERY")
+LOG_TAG_CHOSEN_RESULT = FormattedText().bold("CHOSEN")
+
+
+class LogApi:
+    _instance = None
+
+    @classmethod
+    def get(cls, logger: Logger):
+        """:rtype: LogApi"""
+        if cls._instance is None or logger is not cls._instance.logger:
+            cls._instance = LogApi(logger, LogFormatter())
+        return cls._instance
+
+    def __init__(self, logger: Logger, formatter: LogFormatter):
+        self.logger = logger
+        self.formatter = formatter
+
+    def log_query(self, query: ApiObject, time_point: TimePoint, locale: Locale, results_found: list,
+                  results_sent: list, processing_seconds: float):
+
+        formatted_time_point = self.formatter.time_point(time_point.id())
+        formatted_user = self.formatter.user(query.from_)
+        formatted_query = self.formatter.query(query.query, query.offset)
+        formatted_locale = self.formatter.locale(locale)
+        formatted_processing_time = self.formatter.processing_time(processing_seconds)
+        formatted_results_number = self.formatter.results(results_sent, results_found)
+
+        self.logger.log(
+            LOG_TAG_QUERY,
+            formatted_time_point,
+            formatted_user,
+            formatted_query,
+            formatted_locale,
+            formatted_processing_time,
+            formatted_results_number
+        )
+
+    def log_chosen_result(self, user: ApiObject, time_point: str, chosen_zone_name: str, query: str):
+
+        formatted_time_point = self.formatter.time_point(time_point)
+        formatted_user = self.formatter.user(user)
+        formatted_query = self.formatter.query(query, "")
+        formatted_chosen_zone = self.formatter.chosen_zone(chosen_zone_name)
+
+        self.logger.log(
+            LOG_TAG_CHOSEN_RESULT,
+            formatted_time_point,
+            formatted_user,
+            formatted_query,
+            formatted_chosen_zone
+        )
---
+++ clock/log/formatter.py
@@ -0,0 +1,50 @@
+from babel import Locale
+from bot.action.util.format import UserFormatter, TimeFormatter
+from bot.action.util.textformat import FormattedText
+from bot.api.domain import ApiObject
+
+
+class LogFormatter:
+    @staticmethod
+    def time_point(time_point: str):
+        return FormattedText().normal("Time: {time_point}").start_format().bold(time_point=time_point).end_format()
+
+    @staticmethod
+    def user(user: ApiObject):
+        formatted_user = FormattedText().normal("From: {user}").start_format()\
+            .bold(user=UserFormatter(user).full_format).end_format()
+        user_language_code = user.language_code
+        if user_language_code:
+            formatted_user.concat(
+                FormattedText().normal(" ({language_code})").start_format()
+                    .bold(language_code=user_language_code).end_format()
+            )
+        return formatted_user
+
+    @staticmethod
+    def query(query: str, offset: str):
+        formatted_query = FormattedText().normal("Query: {query}").start_format().bold(query=query).end_format()
+        if offset:
+            formatted_query.concat(
+                FormattedText().normal(" (offset: {offset})").start_format().bold(offset=offset).end_format()
+            )
+        return formatted_query
+
+    @staticmethod
+    def locale(locale: Locale):
+        return FormattedText().normal("Locale: {locale}").start_format().bold(locale=str(locale)).end_format()
+
+    @staticmethod
+    def processing_time(processing_seconds: float):
+        return FormattedText().normal("Processing time: {processing_time}").start_format()\
+            .bold(processing_time=TimeFormatter.format(processing_seconds)).end_format()
+
+    @staticmethod
+    def results(results_sent: list, results_found: list):
+        return FormattedText().normal("{results_sent} results sent / {results_found} results found")\
+            .start_format().bold(results_sent=len(results_sent), results_found=len(results_found)).end_format()
+
+    @staticmethod
+    def chosen_zone(chosen_zone_name: str):
+        return FormattedText().normal("Chosen zone: {zone_name}")\
+            .start_format().bold(zone_name=chosen_zone_name).end_format()
