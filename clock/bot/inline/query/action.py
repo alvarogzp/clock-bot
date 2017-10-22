31aca4c8d93c2f27e48baa62fb6cb8790efc03ab
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-22 03:28:08 +0200

    Update inline query action to use ResultFormatter

--- clock/bot/inline/query/action.py
+++ clock/bot/inline/query/action.py
@@ -2,10 +2,8 @@ from babel import Locale
 from bot.action.core.action import Action
 from bot.multithreading.work import Work
 
-from clock.bot.inline.query.result.formatter.date_time_zone import DateTimeZoneResultFormatter
-from clock.domain.datetimezone import DateTimeZone, DateTimeZoneFormatter
+from clock.bot.inline.query.result.generator import ResultGenerator
 from clock.domain.time import TimePoint
-from clock.domain.zone import Zone
 from clock.finder.api import ZoneFinderApi
 from clock.log.api import LogApi
 from clock.storage.api import StorageApi
@@ -34,7 +32,7 @@ class InlineQueryClockAction(Action):
         offset_end = offset + MAX_RESULTS_PER_QUERY
         next_offset = self.__get_next_offset(len(zones), offset_end)
 
-        results = self.__get_results(current_time, locale, zones[offset:offset_end])
+        results = ResultGenerator.generate(current_time, locale, zones[offset:offset_end])
 
         processing_time = TimePoint.current_timestamp() - current_time.timestamp
 
@@ -71,13 +69,3 @@ class InlineQueryClockAction(Action):
         if result_number > offset_end:
             return str(offset_end)
         return None
-
-    def __get_results(self, time_point: TimePoint, locale: Locale, zones: list):
-        return [self.__get_result(time_point, zone, locale) for zone in zones]
-
-    @staticmethod
-    def __get_result(time_point: TimePoint, zone: Zone, locale: Locale):
-        date_time_zone = DateTimeZone(time_point, zone)
-        date_time_zone_formatter = DateTimeZoneFormatter(date_time_zone, locale)
-        inline_date_time_zone_result_formatter = DateTimeZoneResultFormatter(date_time_zone_formatter)
-        return inline_date_time_zone_result_formatter.result()
