357e43f1fdb0edd00222ae216dc2967942177138
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-23 04:26:33 +0200

    Remove RootSearchStrategy and replace it with a SearchStrategyFactory

--- clock/finder/api.py
+++ clock/finder/api.py
@@ -3,13 +3,14 @@ from collections import OrderedDict
 from babel import Locale
 
 from clock.domain.time import TimePoint
-from clock.finder.search_strategies.search_strategies.root import RootSearchStrategy
+from clock.finder.search_strategies.search_strategies.factory import SearchStrategyFactory
 from clock.finder.zone_finder.provider import ZoneFindersProvider
 
 
 class ZoneFinderApi:
     def __init__(self, find_countries: bool):
-        self.finders = ZoneFindersProvider(find_countries)
+        finders = ZoneFindersProvider(find_countries)
+        self.search_strategy_factory = SearchStrategyFactory(finders)
 
     def find(self, query: str, locale: Locale, time_point: TimePoint):
         search_strategy = self.__get_search_strategy(query, locale, time_point)
@@ -17,7 +18,7 @@ class ZoneFinderApi:
         return self.__removed_duplicates(search_strategy.get_results())
 
     def __get_search_strategy(self, query: str, locale: Locale, time_point: TimePoint):
-        return RootSearchStrategy(self.finders, locale, query, time_point)
+        return self.search_strategy_factory.get(query, locale, time_point)
 
     @staticmethod
     def __removed_duplicates(results):
