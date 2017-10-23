7ea470cae9593c8c7b9711228e9f84814339638a
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-23 04:06:26 +0200

    Convert MatchSearchStrategy to a base class managing the prioritized results

--- clock/finder/search_strategies/search_strategies/query/match/__init__.py
+++ clock/finder/search_strategies/search_strategies/query/match/__init__.py
@@ -1,27 +1,22 @@
 import itertools
 
-from babel import Locale
-
-from clock.domain.time import TimePoint
-from clock.finder.search_strategies.strategy import SearchStrategy
-from clock.finder.zone_finder.provider import ZoneFindersProvider
+from clock.finder.search_strategies.search_strategies.query import QuerySearchStrategy
 
 
-class MatchSearchStrategy(SearchStrategy):
-    def __init__(self, finders: ZoneFindersProvider, locale: Locale, query_lower: str, time_point: TimePoint):
-        self.finders = finders
-        self.locale = locale
-        self.query_lower = query_lower
-        self.time_point = time_point
-        self.localized_date_time_zone_finder = self.finders.localized_date_time_zone_finder(self.locale, self.time_point)
+class MatchSearchStrategy(QuerySearchStrategy):
+    def __init__(self, query_lower: str):
+        super().__init__(query_lower)
         self.prioritized_results = [[] for _ in range(3)]
 
     def search(self):
-        pass
+        raise NotImplementedError()
 
-    def __add_results(self, results):
+    def _add_results(self, results):
         for prioritized_result_list, result_list in zip(self.prioritized_results, results):
             prioritized_result_list.extend(result_list)
 
     def get_results(self):
         return itertools.chain.from_iterable(self.prioritized_results)
+
+    def get_prioritized_results(self):
+        return self.prioritized_results
