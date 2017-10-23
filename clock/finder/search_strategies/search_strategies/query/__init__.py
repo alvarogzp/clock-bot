58a9dc6f8beba21452ab10da37c6d0a5cbc89b02
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-23 04:13:47 +0200

    Convert QuerySearchStrategy into a base class handling the query and the results

--- clock/finder/search_strategies/search_strategies/query/__init__.py
+++ clock/finder/search_strategies/search_strategies/query/__init__.py
@@ -1,21 +1,13 @@
-from babel import Locale
-
-from clock.domain.time import TimePoint
-from clock.finder.search_strategies.search_strategies.query.match import MatchSearchStrategy
 from clock.finder.search_strategies.strategy import SearchStrategy
-from clock.finder.zone_finder.provider import ZoneFindersProvider
 
 
 class QuerySearchStrategy(SearchStrategy):
-    def __init__(self, finders: ZoneFindersProvider, locale: Locale, query_lower: str, time_point: TimePoint):
-        self.finders = finders
-        self.locale = locale
+    def __init__(self, query_lower: str):
         self.query_lower = query_lower
-        self.time_point = time_point
         self.results = []
 
     def search(self):
-        pass
+        raise NotImplementedError()
 
     def get_results(self):
         return self.results
