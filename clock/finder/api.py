7ac7f7863bf2b6663058f3421db731293f2e0392
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-27 10:18:45 +0200

    Create a ZoneFinderLocaleCache class to handle locale cache in ZoneFindersApi. Add a cache() method to the api that returns it.

--- clock/finder/api.py
+++ clock/finder/api.py
@@ -4,18 +4,22 @@ from babel import Locale
 
 from clock.domain.time import TimePoint
 from clock.finder.search_strategies.search_strategies.factory import SearchStrategyFactory
-from clock.finder.zone_finder.provider import ZoneFindersProvider
+from clock.finder.zone_finder.provider import ZoneFindersProvider, LocalizedZoneFinderCache
 
 
 class ZoneFinderApi:
     def __init__(self, find_countries: bool):
         finders = ZoneFindersProvider(find_countries)
         self.search_strategy_factory = SearchStrategyFactory(finders)
+        self.locale_cache = ZoneFinderLocaleCache(finders.get_localized_zone_finder_cache())
 
     def find(self, query: str, locale: Locale, time_point: TimePoint):
         search_strategy = self.__get_search_strategy(query, locale, time_point)
         search_strategy.search()
         return self.__removed_duplicates(search_strategy.get_results())
+
+    def cache(self):
+        return self.locale_cache
 
     def __get_search_strategy(self, query: str, locale: Locale, time_point: TimePoint):
         return self.search_strategy_factory.get(query, locale, time_point)
@@ -23,3 +27,17 @@ class ZoneFinderApi:
     @staticmethod
     def __removed_duplicates(results):
         return list(OrderedDict.fromkeys(results))
+
+
+class ZoneFinderLocaleCache:
+    def __init__(self, cache: LocalizedZoneFinderCache):
+        self.cache = cache
+
+    def cache(self, locale: Locale):
+        self.cache.get_or_generate(locale)
+
+    def cached_locales(self):
+        return self.cache.cached_locales()
+
+    def is_cached(self, locale: Locale):
+        return self.cache.is_cached(locale)
