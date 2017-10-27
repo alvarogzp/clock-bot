5d1ac9bb28f4bde2b855fa192ba31901a83bd820
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-27 09:53:56 +0200

    Create SynchronizedCache as a Cache subclass

--- clock/util.py
+++ clock/util.py
@@ -1,3 +1,7 @@
+import threading
+from collections import defaultdict
+
+
 class Cache:
     def __init__(self):
         self.cache = {}
@@ -7,3 +11,21 @@ class Cache:
         if value is None:
             self.cache[key] = value = generate_func()
         return value
+
+
+class SynchronizedCache(Cache):
+    def __init__(self):
+        super().__init__()
+        # one lock per key
+        self.locks = defaultdict(threading.Lock)
+
+    def get_or_generate(self, key, generate_func: callable):
+        # quick path: if value is in dict, return it
+        value = self.cache.get(key)
+        if value is not None:
+            return value
+        # slow path: value was not in dict, get lock to be sure to generate it only once
+        with self.locks[key]:
+            # with lock acquired we need to check again if key is in dict before inserting
+            # because it could have been added between the unlocked check and the lock acquire
+            return super().get_or_generate(key, generate_func)
