cf439e0763f6c1c09595ed29224bd1fd743592d3
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-03 13:00:39 +0100

    Update StartAction to use new MessageWithReplyMarkupBuilder

--- clock/bot/commands/start.py
+++ clock/bot/commands/start.py
@@ -1,28 +1,41 @@
+from bot.action.core.action import Action
 from bot.action.util.textformat import FormattedText
+from bot.storage import Cache
 
-from clock.bot.commands.util.static_response import StaticResponseAction
+from clock.bot.commands.util.message_builder import MessageWithReplyMarkupBuilder
 
 
-class StartAction(StaticResponseAction):
-    def build_message(self):
-        text = self._get_text()
-        message = text.build_message()
-        message.data["reply_markup"] = self._get_reply_markup()
-        return message
+class StartAction(Action):
+    def __init__(self):
+        super().__init__()
+        self.start_message = None  # initialized in post_setup
 
-    def _get_text(self):
+    def post_setup(self):
+        self.start_message = StartMessageBuilder(self.cache).get_message()
+
+    def process(self, event):
+        self.api.async.send_message(self.start_message.to_chat_replying(event.message))
+
+
+class StartMessageBuilder(MessageWithReplyMarkupBuilder):
+    def __init__(self, cache: Cache):
+        self.cache = cache
+
+    def get_text(self):
         return FormattedText()\
             .bold("👋 Hello! 👋").newline().newline()\
             .normal("I am {name}.").newline()\
             .bold("I can tell you the current time of any place in the world.").newline()\
             .normal("You have to use me in inline mode.").newline().newline()\
             .bold("👇 Tap any button below to try me 👇")\
-            .start_format().bold(name=self._get_bot_name()).end_format()
+            .start_format()\
+            .bold(name=self._get_bot_name())\
+            .end_format()
 
     def _get_bot_name(self):
         return self.cache.bot_info.first_name
 
-    def _get_reply_markup(self):
+    def get_reply_markup(self):
         return {
             "inline_keyboard": [
                 [self.switch_inline_button("What time is it in New York?", "New York", current_chat=True)],
