9cdc34af32a0853068a8bc288c4b7bd8620068b4
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-03 14:14:05 +0100

    Update message_builder to use new with_reply_markup method of Message, and remove now unneeded switch_inline_button (as telegram-bot-framework now implements it)

--- clock/bot/commands/util/message_builder.py
+++ clock/bot/commands/util/message_builder.py
@@ -1,20 +1,11 @@
 class MessageWithReplyMarkupBuilder:
     def get_message(self):
         text = self.get_text()
-        message = text.build_message()
-        message.data["reply_markup"] = self.get_reply_markup()
-        return message
+        reply_markup = self.get_reply_markup()
+        return text.build_message().with_reply_markup(reply_markup)
 
     def get_text(self):
         raise NotImplementedError()
 
     def get_reply_markup(self):
         raise NotImplementedError()
-
-    @staticmethod
-    def switch_inline_button(text: str, query: str, current_chat: bool = True):
-        switch_inline_query_key = "switch_inline_query_current_chat" if current_chat else "switch_inline_query"
-        return {
-            "text": text,
-            switch_inline_query_key: query
-        }
