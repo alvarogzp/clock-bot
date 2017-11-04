d1af17c2b14698c48f8e9fefc8ab0792238f3acc
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-04 17:44:10 +0100

    Use the troubleshoot message in StartAction if it is called with a no_results param

--- clock/bot/commands/start.py
+++ clock/bot/commands/start.py
@@ -3,6 +3,9 @@ from bot.action.core.action import Action
 from clock.bot.commands.util import messages
 
 
+NO_RESULTS_PARAMETER = "no_results"
+
+
 class StartAction(Action):
     def __init__(self):
         super().__init__()
@@ -12,4 +15,8 @@ class StartAction(Action):
         self.start_message = messages.start(self.cache.bot_info)
 
     def process(self, event):
-        self.api.async.send_message(self.start_message.copy().to_chat_replying(event.message))
+        if event.command_args == NO_RESULTS_PARAMETER:
+            message = messages.troubleshoot(event.message.from_, self.cache.zone_finder)
+        else:
+            message = self.start_message.copy()
+        self.api.async.send_message(message.to_chat_replying(event.message))
