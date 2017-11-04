98192c5f238ac94e05490b25c0f7beef4a3b1b41
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-04 17:29:41 +0100

    Add troubleshoot to messsages __init__ module

--- clock/bot/commands/util/messages/__init__.py
+++ clock/bot/commands/util/messages/__init__.py
@@ -1,8 +1,10 @@
 from bot.storage import Cache
+from bot.api.domain import ApiObject
 
 from clock.bot.commands.util.messages.help import HelpMessageBuilder
-from clock.bot.commands.util.messages.no_results import NoResultsMessageBuilder
 from clock.bot.commands.util.messages.start import StartMessageBuilder
+from clock.bot.commands.util.messages.troubleshoot import TroubleshootMessageBuilder
+from clock.finder.api import ZoneFinderApi
 
 
 def start(cache: Cache):
@@ -11,3 +13,7 @@ def start(cache: Cache):
 
 def help(cache: Cache):
     return HelpMessageBuilder(cache).get_message()
+
+
+def troubleshoot(user: ApiObject, zone_finder: ZoneFinderApi):
+    return TroubleshootMessageBuilder(user, zone_finder).get_message()
