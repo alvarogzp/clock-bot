c6cf8f26c225efcde1916fd429c28e91e3b4df19
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-07 16:30:24 +0100

    Create SqliteStorageComponentFactory

---
+++ clock/storage/data_source/data_sources/sqlite/component/factory.py
@@ -0,0 +1,15 @@
+from sqlite3 import Connection
+
+from clock.storage.data_source.data_sources.sqlite import QuerySqliteComponent
+from clock.storage.data_source.data_sources.sqlite.component.components.user import UserSqliteComponent
+
+
+class SqliteStorageComponentFactory:
+    def __init__(self, connection: Connection):
+        self.connection = connection
+
+    def user(self):
+        return UserSqliteComponent(self.connection)
+
+    def query(self):
+        return QuerySqliteComponent(self.connection)
