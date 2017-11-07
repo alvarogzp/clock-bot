d02d2509bd02459638048029a151035de1029add
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-07 18:42:17 +0100

    Add save_chat and save_message to sqlite data source using chat and message components

--- clock/storage/data_source/data_sources/sqlite/sqlite.py
+++ clock/storage/data_source/data_sources/sqlite/sqlite.py
@@ -2,6 +2,8 @@ import sqlite3
 
 from clock.storage.data_source.data_source import StorageDataSource
 from clock.storage.data_source.data_sources.sqlite.component.component import SqliteStorageComponent
+from clock.storage.data_source.data_sources.sqlite.component.components.chat import ChatSqliteComponent
+from clock.storage.data_source.data_sources.sqlite.component.components.message import MessageSqliteComponent
 from clock.storage.data_source.data_sources.sqlite.component.components.query import QuerySqliteComponent
 from clock.storage.data_source.data_sources.sqlite.component.components.user import UserSqliteComponent
 from clock.storage.data_source.data_sources.sqlite.component.factory import SqliteStorageComponentFactory
@@ -15,14 +17,18 @@ class SqliteStorageDataSource(StorageDataSource):
         # initialized in init to avoid creating sqlite objects outside the thread in which it will be operating
         self.connection = None
         self.user = None  # type: UserSqliteComponent
+        self.chat = None  # type: ChatSqliteComponent
         self.query = None  # type: QuerySqliteComponent
+        self.message = None  # type: MessageSqliteComponent
 
     def init(self):
         self.connection = sqlite3.connect(DATABASE_FILENAME)
         self.connection.row_factory = sqlite3.Row  # improved rows
         components = SqliteStorageComponentFactory(self.connection)
         self.user = self._get_and_init(components.user())
+        self.chat = self._get_and_init(components.chat())
         self.query = self._get_and_init(components.query())
+        self.message = self._get_and_init(components.message())
 
     @staticmethod
     def _get_and_init(component: SqliteStorageComponent):
@@ -31,6 +37,9 @@ class SqliteStorageDataSource(StorageDataSource):
 
     def save_user(self, user_id: int, first_name: str, last_name: str, username: str, language_code: str):
         self.user.save_user(user_id, first_name, last_name, username, language_code)
+
+    def save_chat(self, chat_id: int, chat_type: str, title: str, username: str):
+        self.chat.save_chat(chat_id, chat_type, title, username)
 
     def save_query(self, user_id: int, timestamp: str, query: str, offset: str, locale: str, results_found_len: int,
                    results_sent_len: int, processing_seconds: float):
@@ -41,6 +50,9 @@ class SqliteStorageDataSource(StorageDataSource):
     def save_chosen_result(self, user_id: int, timestamp: str, chosen_zone_name: str, query: str,
                            choosing_seconds: float):
         self.query.save_chosen_result(user_id, timestamp, chosen_zone_name, query, choosing_seconds)
+
+    def save_message(self, chat_id: int, message_id: int, user_id: int, date: int, text: str):
+        self.message.save_message(chat_id, message_id, user_id, date, text)
 
     def commit(self):
         self.connection.commit()
