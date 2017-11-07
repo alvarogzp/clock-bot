ac84654270321967306442c1e76f6a389c9b4e87
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-07 14:25:46 +0100

    Add a StorageScheduler to StorageApi, and schedule save operations on it

--- clock/storage/api.py
+++ clock/storage/api.py
@@ -2,6 +2,7 @@ from babel import Locale
 from bot.api.domain import ApiObject
 
 from clock.domain.time import TimePoint
+from clock.storage.async.scheduler import StorageScheduler
 from clock.storage.data_source.data_source import StorageDataSource
 from clock.storage.data_source.data_sources.sqlite import SqliteStorageDataSource
 
@@ -16,11 +17,24 @@ class StorageApi:
             cls._instance = StorageApi(SqliteStorageDataSource())
         return cls._instance
 
-    def __init__(self, data_source: StorageDataSource):
+    def __init__(self, data_source: StorageDataSource, scheduler: StorageScheduler):
         self.data_source = data_source
+        self.scheduler = scheduler
 
     def save_query(self, query: ApiObject, time_point: TimePoint, locale: Locale, results_found: list,
                    results_sent: list, processing_seconds: float):
+        self.scheduler.schedule_no_result(
+            lambda: self._save_query(query, time_point, locale, results_found, results_sent, processing_seconds)
+        )
+
+    def save_chosen_result(self, user: ApiObject, timestamp: str, chosen_zone_name: str, query: str,
+                           choosing_seconds: float):
+        self.scheduler.schedule_no_result(
+            lambda: self._save_chosen_result(user, timestamp, chosen_zone_name, query, choosing_seconds)
+        )
+
+    def _save_query(self, query: ApiObject, time_point: TimePoint, locale: Locale, results_found: list,
+                    results_sent: list, processing_seconds: float):
         self._save_user(query.from_)
         self.data_source.save_query(query.from_.id, time_point.id(), query.query, query.offset, str(locale),
                                     len(results_found), len(results_sent), processing_seconds)
@@ -29,8 +43,8 @@ class StorageApi:
     def _save_user(self, user: ApiObject):
         self.data_source.save_user(user.id, user.first_name, user.last_name, user.username, user.language_code)
 
-    def save_chosen_result(self, user: ApiObject, timestamp: str, chosen_zone_name: str, query: str,
-                           choosing_seconds: float):
+    def _save_chosen_result(self, user: ApiObject, timestamp: str, chosen_zone_name: str, query: str,
+                            choosing_seconds: float):
         # the query might have been retrieved from server-side cache and the user could be unknown for us
         self._save_user(user)
         self.data_source.save_chosen_result(user.id, timestamp, chosen_zone_name, query, choosing_seconds)
