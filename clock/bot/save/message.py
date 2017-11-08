a67350805a14da4a5b384c062e621020f3017d6d
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-08 18:12:19 +0100

    Modify SaveMessageAction to check if the chat has become inactive by a migration or because the bot is no longer in it

--- clock/bot/save/message.py
+++ clock/bot/save/message.py
@@ -12,4 +12,23 @@ class SaveMessageAction(Action):
         self.storage = self.cache.storage
 
     def process(self, event):
-        self.storage.save_message(event.message)
+        message = event.message
+        self.storage.save_message(message)
+        self._check_inactive(message)
+
+    def _check_inactive(self, message):
+        self._check_left_chat(message)
+        self._check_chat_migrated(message)
+
+    def _check_left_chat(self, message):
+        left_chat_member = message.left_chat_member
+        if left_chat_member is not None and left_chat_member.id == self.cache.bot_info.id:
+            from_ = message.from_
+            reason = "left, by: " + str(from_.id) if from_ is not None else "unknown"
+            self.storage.set_inactive_chat(message.chat, reason)
+
+    def _check_chat_migrated(self, message):
+        migrate_to_chat_id = message.migrate_to_chat_id
+        if migrate_to_chat_id is not None:
+            reason = "migrated to: " + str(migrate_to_chat_id)
+            self.storage.set_inactive_chat(message.chat, reason)
