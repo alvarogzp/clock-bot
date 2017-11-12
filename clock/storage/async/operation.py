8ee02b60bb15e79a287308e56a2e31785ff9d8e9
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-12 19:32:30 +0100

    Run storage operations inside a context manager

--- clock/storage/async/operation.py
+++ clock/storage/async/operation.py
@@ -5,8 +5,9 @@ from bot.multithreading.worker import Worker
 
 
 class StorageOperation:
-    def __init__(self, worker: Worker, func: callable, ignore_result: bool):
+    def __init__(self, worker: Worker, context_manager, func: callable, ignore_result: bool):
         self.worker = worker
+        self.context_manager = context_manager
         self.func = func
         self.ignore_result = ignore_result
         self.result_queue = queue.Queue() if not ignore_result else None
@@ -26,9 +27,13 @@ class StorageOperation:
     def _wrapper_func(self):
         result = None
         try:
-            result = self.func()
+            result = self._run_func_in_context_manager()
         finally:
             if not self.ignore_result:
                 # always put a result, even if the operation crashes,
                 # to avoid caller from getting deadlocked waiting for the result
                 self.result_queue.put(result)
+
+    def _run_func_in_context_manager(self):
+        with self.context_manager:
+            return self.func()
