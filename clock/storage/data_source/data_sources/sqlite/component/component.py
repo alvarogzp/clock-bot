c87576fd4da111823df6e0961404738964e85af5
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-23 06:50:40 +0100

    Document the unsafe nature of _add_columns

--- clock/storage/data_source/data_sources/sqlite/component/component.py
+++ clock/storage/data_source/data_sources/sqlite/component/component.py
@@ -14,10 +14,17 @@ class SqliteStorageComponent:
         raise NotImplementedError()
 
     def _add_columns(self, table: str, *columns: str):
+        """
+        IMPORTANT:
+        Table name and column definitions are added to the sql statement in an unsafe way!
+        So, untrusted input MUST NOT be passed to them.
+        Their values should ideally be static string literals.
+        If computed at runtime, they MUST come from a TOTALLY trusted source (like another module string constant
+        or an admin-controlled configuration value).
+        """
         for column in columns:
             # table name and column definitions cannot be (safely) parametrized by the sqlite engine
-            # but as we trust the input (the caller use hardcoded params),
-            # we can format the statement in an unsafe way
+            # but as we trust the input, we can format the statement in an unsafe way
             sql = "alter table {table} add column {column}".format(table=table, column=column)
             self.sql(sql)
 
