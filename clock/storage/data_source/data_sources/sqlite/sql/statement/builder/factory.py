ff619f9a9de9acc69040809da82cfdb399247b40
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-24 13:58:44 +0100

    Create statement factory

---
+++ clock/storage/data_source/data_sources/sqlite/sql/statement/builder/factory.py
@@ -0,0 +1,23 @@
+from sqlite3 import Connection
+
+from clock.storage.data_source.data_sources.sqlite.sql.statement.builder.alter_table import AlterTableBuilder
+from clock.storage.data_source.data_sources.sqlite.sql.statement.builder.create_table import CreateTableBuilder
+from clock.storage.data_source.data_sources.sqlite.sql.statement.builder.select import SelectBuilder
+
+
+class StatementBuilderFactory:
+    def __init__(self, connection: Connection):
+        self.connection = connection
+
+    def select(self) -> SelectBuilder:
+        return self._initialized(SelectBuilder())
+
+    def create_table(self) -> CreateTableBuilder:
+        return self._initialized(CreateTableBuilder())
+
+    def alter_table(self) -> AlterTableBuilder:
+        return self._initialized(AlterTableBuilder())
+
+    def _initialized(self, builder):
+        builder.set_connection(self.connection)
+        return builder
