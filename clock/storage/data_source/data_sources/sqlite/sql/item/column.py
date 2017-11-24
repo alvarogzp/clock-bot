55c23eb209730871a7d57b5c808a020e1d942ab9
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-24 13:13:01 +0100

    Add Column sql item

---
+++ clock/storage/data_source/data_sources/sqlite/sql/item/column.py
@@ -0,0 +1,15 @@
+from clock.storage.data_source.data_sources.sqlite.sql.item.base import SqlItem
+
+
+class Column(SqlItem):
+    def __init__(self, name: str, type: str, *constraints: str):
+        self.name = name
+        self.type = type
+        self.constraints = constraints
+
+    def str(self):
+        column = "{name} {type}".format(name=self.name, type=self.type)
+        if self.constraints:
+            constraints = " ".join(self.constraints)
+            column += " {constraints}".format(constraints=constraints)
+        return column
