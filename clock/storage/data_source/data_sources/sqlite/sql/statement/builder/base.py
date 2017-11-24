309dc382f0649f9a80a8bc7d8d8c8c88d755a570
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-24 13:40:51 +0100

    Create statement builder

---
+++ clock/storage/data_source/data_sources/sqlite/sql/statement/builder/base.py
@@ -0,0 +1,26 @@
+from sqlite3 import Connection
+
+from clock.storage.data_source.data_sources.sqlite.sql.statement.statement import CompoundSqlStatement, \
+    SingleSqlStatement
+
+
+class StatementBuilder:
+    def __init__(self, multiple_statements: bool = False):
+        self.multiple_statements = multiple_statements
+        self.connection = None  # type: Connection
+
+    def set_connection(self, connection: Connection):
+        self.connection = connection
+
+    def build_sql(self):
+        raise NotImplementedError()
+
+    def build(self):
+        sql = self.build_sql()
+        if self.multiple_statements:
+            return CompoundSqlStatement(self.connection, sql)
+        return SingleSqlStatement(self.connection, sql)
+
+    def execute(self, *args, **kwargs):
+        """Convenience method that builds and executes the statement in a single call"""
+        return self.build().execute(*args, **kwargs)
