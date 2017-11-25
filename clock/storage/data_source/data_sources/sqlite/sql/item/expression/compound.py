319c04ae5455970beb3fe5256f606260a58a460e
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-25 13:18:15 +0100

    Create a CompoundExpression with a wrap method and make Condition extend it

--- clock/storage/data_source/data_sources/sqlite/sql/item/expression/compound.py
+++ clock/storage/data_source/data_sources/sqlite/sql/item/expression/compound.py
@@ -7,15 +7,20 @@ from clock.storage.data_source.data_sources.sqlite.sql.item.expression.parser im
 OPERATOR_EQUAL = "="
 
 
-class Condition(Expression):
-    def __init__(self, left: Union[Expression, Any], operator: str, right: Union[Expression, Any]):
-        self.left = self._wrap_if_not_expression(left)
-        self.operator = operator
-        self.right = self._wrap_if_not_expression(right)
+class CompoundExpression(Expression):
+    def str(self):
+        raise NotImplementedError()
 
     @staticmethod
-    def _wrap_if_not_expression(expr):
+    def wrap(expr):
         return ExpressionParser.parse(expr)
+
+
+class Condition(CompoundExpression):
+    def __init__(self, left: Union[Expression, Any], operator: str, right: Union[Expression, Any]):
+        self.left = self.wrap(left)
+        self.operator = operator
+        self.right = self.wrap(right)
 
     def str(self):
         return "{left} {operator} {right}"\
