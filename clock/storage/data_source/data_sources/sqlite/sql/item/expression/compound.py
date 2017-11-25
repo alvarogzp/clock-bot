209181dff06b5545ec154130af765acb029929fa
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-25 13:19:11 +0100

    Create a Cast compound expression

--- clock/storage/data_source/data_sources/sqlite/sql/item/expression/compound.py
+++ clock/storage/data_source/data_sources/sqlite/sql/item/expression/compound.py
@@ -2,6 +2,7 @@ from typing import Union, Any
 
 from clock.storage.data_source.data_sources.sqlite.sql.item.expression.base import Expression
 from clock.storage.data_source.data_sources.sqlite.sql.item.expression.parser import ExpressionParser
+from clock.storage.data_source.data_sources.sqlite.sql.item.type import Type
 
 
 OPERATOR_EQUAL = "="
@@ -25,3 +26,12 @@ class Condition(CompoundExpression):
     def str(self):
         return "{left} {operator} {right}"\
             .format(left=self.left.str(), operator=self.operator, right=self.right.str())
+
+
+class Cast(CompoundExpression):
+    def __init__(self, expr: Union[Expression, Any], type: Type):
+        self.expr = self.wrap(expr)
+        self.type = type
+
+    def str(self):
+        return "cast({expr} as {type})".format(expr=self.expr.str(), type=self.type.str())
