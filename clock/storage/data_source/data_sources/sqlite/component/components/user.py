7d04a6bacaf35ef8a3fbeabd000ed84e164e8723
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-25 14:59:13 +0100

    Update user component replacing string literals with column references and using new sql framework updates

--- clock/storage/data_source/data_sources/sqlite/component/components/user.py
+++ clock/storage/data_source/data_sources/sqlite/component/components/user.py
@@ -1,39 +1,44 @@
 from clock.storage.data_source.data_sources.sqlite.component.component import SqliteStorageComponent
-from clock.storage.data_source.data_sources.sqlite.sql.item.column import Column
+from clock.storage.data_source.data_sources.sqlite.sql.item.column import Column, COLUMN_ROWID
+from clock.storage.data_source.data_sources.sqlite.sql.item.constants.operator import OPERATOR_EQUAL
+from clock.storage.data_source.data_sources.sqlite.sql.item.constants.order_mode import ORDER_DESC
+from clock.storage.data_source.data_sources.sqlite.sql.item.constants.type import TYPE_INTEGER
+from clock.storage.data_source.data_sources.sqlite.sql.item.expression.compound import Condition, Cast
 from clock.storage.data_source.data_sources.sqlite.sql.item.table import Table
 from clock.storage.data_source.data_sources.sqlite.sql.schema.table import TableSchema
 
 
-COLUMN_NAME_USER_ID = "user_id"
-COLUMN_NAME_FIRST_NAME = "first_name"
-COLUMN_NAME_LAST_NAME = "last_name"
-COLUMN_NAME_USERNAME = "username"
-COLUMN_NAME_LANGUAGE_CODE = "language_code"
-COLUMN_NAME_IS_BOT = "is_bot"
-COLUMN_NAME_TIMESTAMP_ADDED = "timestamp_added"
-COLUMN_NAME_TIMESTAMP_REMOVED = "timestamp_removed"
+COLUMN_USER_ID = Column("user_id", "integer", "primary key", "not null")
+COLUMN_FIRST_NAME = Column("first_name", "text")
+COLUMN_LAST_NAME = Column("last_name", "text")
+COLUMN_USERNAME = Column("username", "text")
+COLUMN_LANGUAGE_CODE = Column("language_code", "text")
+COLUMN_IS_BOT = Column("is_bot", "integer")  # boolean
+COLUMN_TIMESTAMP_ADDED = Column("timestamp_added", "text")
+COLUMN_USER_ID_USER_HISTORY = Column("user_id", "integer", "not null")
+COLUMN_TIMESTAMP_REMOVED = Column("timestamp_removed", "text")
 
 
 USER = TableSchema()
 USER.table = Table("user")
-USER.column(Column(COLUMN_NAME_USER_ID, "integer", "primary key", "not null"))
-USER.column(Column(COLUMN_NAME_FIRST_NAME, "text"))
-USER.column(Column(COLUMN_NAME_LAST_NAME, "text"))
-USER.column(Column(COLUMN_NAME_USERNAME, "text"))
-USER.column(Column(COLUMN_NAME_LANGUAGE_CODE, "text"))
-USER.column(Column(COLUMN_NAME_IS_BOT, "integer"), version=2)  # boolean
-USER.column(Column(COLUMN_NAME_TIMESTAMP_ADDED, "text"))
+USER.column(COLUMN_USER_ID)
+USER.column(COLUMN_FIRST_NAME)
+USER.column(COLUMN_LAST_NAME)
+USER.column(COLUMN_USERNAME)
+USER.column(COLUMN_LANGUAGE_CODE)
+USER.column(COLUMN_IS_BOT, version=2)
+USER.column(COLUMN_TIMESTAMP_ADDED)
 
 USER_HISTORY = TableSchema()
 USER_HISTORY.table = Table("user_history")
-USER_HISTORY.column(Column(COLUMN_NAME_USER_ID, "integer", "not null"))
-USER_HISTORY.column_from(USER, COLUMN_NAME_FIRST_NAME)
-USER_HISTORY.column_from(USER, COLUMN_NAME_LAST_NAME)
-USER_HISTORY.column_from(USER, COLUMN_NAME_USERNAME)
-USER_HISTORY.column_from(USER, COLUMN_NAME_LANGUAGE_CODE)
-USER_HISTORY.column_from(USER, COLUMN_NAME_IS_BOT)
-USER_HISTORY.column_from(USER, COLUMN_NAME_TIMESTAMP_ADDED)
-USER_HISTORY.column(Column(COLUMN_NAME_TIMESTAMP_REMOVED, "text"))
+USER_HISTORY.column(COLUMN_USER_ID_USER_HISTORY)
+USER_HISTORY.column(COLUMN_FIRST_NAME)
+USER_HISTORY.column(COLUMN_LAST_NAME)
+USER_HISTORY.column(COLUMN_USERNAME)
+USER_HISTORY.column(COLUMN_LANGUAGE_CODE)
+USER_HISTORY.column(COLUMN_IS_BOT, version=2)
+USER_HISTORY.column(COLUMN_TIMESTAMP_ADDED)
+USER_HISTORY.column(COLUMN_TIMESTAMP_REMOVED)
 
 
 class UserSqliteComponent(SqliteStorageComponent):
@@ -85,9 +90,9 @@ class UserSqliteComponent(SqliteStorageComponent):
     def get_user_language_code_at(self, user_id: int, timestamp: str):
         table, rowid = self._find_user_at(user_id, timestamp)
         return self.statement.select()\
-            .fields(COLUMN_NAME_LANGUAGE_CODE)\
+            .fields(COLUMN_LANGUAGE_CODE)\
             .table(table)\
-            .where("rowid = :rowid")\
+            .where(Condition(COLUMN_ROWID, OPERATOR_EQUAL, ":rowid"))\
             .execute(rowid=rowid)\
             .first_field()
 
@@ -95,31 +100,30 @@ class UserSqliteComponent(SqliteStorageComponent):
         timestamp = int(timestamp)
         # try with current user info
         user = self.statement.select()\
-            .fields("rowid", COLUMN_NAME_TIMESTAMP_ADDED)\
+            .fields(COLUMN_ROWID, COLUMN_TIMESTAMP_ADDED)\
             .table(USER.table)\
-            .where("{user_id} = :user_id".format(user_id=COLUMN_NAME_USER_ID))\
+            .where(Condition(COLUMN_USER_ID, OPERATOR_EQUAL, ":user_id"))\
             .execute(user_id=user_id)\
             .first()
         if user is None:
             # if the user is not in the user table, we do not know about their
             raise Exception("unknown user: {user_id}".format(user_id=user_id))
-        last_added = user[COLUMN_NAME_TIMESTAMP_ADDED]
+        last_added = user[COLUMN_TIMESTAMP_ADDED]
         if self.__was_valid_at(timestamp, last_added):
-            rowid = user["rowid"]
+            rowid = user[COLUMN_ROWID]
             return USER.table, rowid
         # now iterate the user_history entries for that user
         user_history = self.statement.select()\
-            .fields("rowid", COLUMN_NAME_TIMESTAMP_ADDED, COLUMN_NAME_TIMESTAMP_REMOVED)\
+            .fields(COLUMN_ROWID, COLUMN_TIMESTAMP_ADDED, COLUMN_TIMESTAMP_REMOVED)\
             .table(USER_HISTORY.table)\
-            .where("{user_id} = :user_id".format(user_id=COLUMN_NAME_USER_ID))\
-            .order_by("cast({timestamp_removed} as integer) desc"
-                      .format(timestamp_removed=COLUMN_NAME_TIMESTAMP_REMOVED))\
+            .where(Condition(COLUMN_USER_ID_USER_HISTORY, OPERATOR_EQUAL, ":user_id"))\
+            .order_by(Cast(COLUMN_TIMESTAMP_REMOVED, TYPE_INTEGER), ORDER_DESC)\
             .execute(user_id=user_id)
         for user in user_history:
-            added = user[COLUMN_NAME_TIMESTAMP_ADDED]
-            removed = user[COLUMN_NAME_TIMESTAMP_REMOVED]
+            added = user[COLUMN_TIMESTAMP_ADDED]
+            removed = user[COLUMN_TIMESTAMP_REMOVED]
             if self.__was_valid_at(timestamp, added, removed):
-                rowid = user["rowid"]
+                rowid = user[COLUMN_ROWID]
                 return USER_HISTORY.table, rowid
         raise Exception("user {user_id} was unknown at {timestamp}".format(user_id=user_id, timestamp=timestamp))
 
