d951f1a4617874fda8920191fd74582f7e353aa8
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-25 11:52:10 +0100

    Add update builder

--- clock/storage/data_source/data_sources/sqlite/sql/statement/builder/factory.py
+++ clock/storage/data_source/data_sources/sqlite/sql/statement/builder/factory.py
@@ -3,6 +3,7 @@ from sqlite3 import Connection
 from clock.storage.data_source.data_sources.sqlite.sql.statement.builder.alter_table import AlterTableBuilder
 from clock.storage.data_source.data_sources.sqlite.sql.statement.builder.create_table import CreateTableBuilder
 from clock.storage.data_source.data_sources.sqlite.sql.statement.builder.select import SelectBuilder
+from clock.storage.data_source.data_sources.sqlite.sql.statement.builder.update import UpdateBuilder
 
 
 class StatementBuilderFactory:
@@ -17,6 +18,9 @@ class StatementBuilderFactory:
 
     def alter_table(self) -> AlterTableBuilder:
         return self._initialized(AlterTableBuilder())
+
+    def update(self) -> UpdateBuilder:
+        return self._initialized(UpdateBuilder())
 
     def _initialized(self, builder):
         builder.set_connection(self.connection)
