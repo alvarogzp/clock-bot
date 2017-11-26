93d767a075af9ffa349ca2b12945e75aff7e8eba
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-26 18:53:17 +0100

    Create a BaseCondition that joins expressions with an operator

--- clock/storage/data_source/data_sources/sqlite/sql/item/expression/compound/condition.py
+++ clock/storage/data_source/data_sources/sqlite/sql/item/expression/compound/condition.py
@@ -3,6 +3,21 @@ from clock.storage.data_source.data_sources.sqlite.sql.item.expression.compound.
 from clock.storage.data_source.data_sources.sqlite.sql.item.expression.parser import EXPRESSION_TYPE
 
 
+class BaseCondition(CompoundExpression):
+    def __init__(self, operator: Operator, *expressions: EXPRESSION_TYPE):
+        self.operator = operator
+        self.expressions = [self.parse(expr) for expr in expressions]
+
+    def str(self):
+        operator = " {operator} ".format(operator=self.operator.str())
+        expressions = (
+            "{expr}".format(expr=expr.str())
+            for expr in self.expressions
+        )
+        conditions = operator.join(expressions)
+        return "({conditions})".format(conditions=conditions)
+
+
 class Condition(CompoundExpression):
     def __init__(self, left: EXPRESSION_TYPE, operator: Operator, right: EXPRESSION_TYPE):
         self.left = self.parse(left)
