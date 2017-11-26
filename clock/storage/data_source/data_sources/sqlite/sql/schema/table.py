7bf9c4bf5ba7b03b0f4610f637519edd7323e85b
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-26 19:01:25 +0100

    Remove unused methods in schema

--- clock/storage/data_source/data_sources/sqlite/sql/schema/table.py
+++ clock/storage/data_source/data_sources/sqlite/sql/schema/table.py
@@ -12,9 +12,6 @@ class TableSchema:
     def column(self, column: Column, version: int = 1):
         self.columns.add(column, version)
 
-    def column_from(self, schema: __class__, column_name: str):
-        self.columns.add_from(schema.columns, column_name)
-
 
 class ColumnListSchema:
     def __init__(self):
@@ -24,15 +21,6 @@ class ColumnListSchema:
     def add(self, column: Column, version: int):
         self._columns[column.name] = column
         self._versions[version].append(column)
-
-    def add_from(self, columns: __class__, column_name: str):
-        column = columns._columns[column_name]
-        for version, columns in columns._versions.items():
-            if column in columns:
-                break
-        else:
-            raise Exception("original column version was not found")
-        self.add(column, version)
 
     def get_all(self):
         return self._columns.values()
