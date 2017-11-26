55070d295f9486d4195f6d008bcf285206b43984
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-26 18:58:15 +0100

    Add is, and & or operators

--- clock/storage/data_source/data_sources/sqlite/sql/item/constants/operator.py
+++ clock/storage/data_source/data_sources/sqlite/sql/item/constants/operator.py
@@ -5,4 +5,12 @@ class Operator(NamedItem):
     pass
 
 
+# operators ordered by precedence (from highest to lowest)
+# an empty line means a precedence jump
+
 OPERATOR_EQUAL = Operator("=")
+OPERATOR_IS = Operator("is")
+
+OPERATOR_AND = Operator("and")
+
+OPERATOR_OR = Operator("or")
