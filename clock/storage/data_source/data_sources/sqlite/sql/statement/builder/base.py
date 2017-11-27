7902f0cf88e2441c3acab208a31dc7662686ca9f
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-27 16:07:51 +0100

    Remove the Connection from the StatementBuilder, and the execute convenience method

--- clock/storage/data_source/data_sources/sqlite/sql/statement/builder/base.py
+++ clock/storage/data_source/data_sources/sqlite/sql/statement/builder/base.py
@@ -1,5 +1,3 @@
-from sqlite3 import Connection
-
 from clock.storage.data_source.data_sources.sqlite.sql.statement.statement import CompoundSqlStatement, \
     SingleSqlStatement
 
@@ -7,10 +5,6 @@ from clock.storage.data_source.data_sources.sqlite.sql.statement.statement impor
 class StatementBuilder:
     def __init__(self):
         self.multiple_statements = False
-        self.connection = None  # type: Connection
-
-    def set_connection(self, connection: Connection):
-        self.connection = connection
 
     def build_sql(self):
         raise NotImplementedError()
@@ -18,12 +12,8 @@ class StatementBuilder:
     def build(self):
         sql = self.build_sql()
         if self.multiple_statements:
-            return CompoundSqlStatement(self.connection, sql)
-        return SingleSqlStatement(self.connection, sql)
-
-    def execute(self, *args, **kwargs):
-        """Convenience method that builds and executes the statement in a single call"""
-        return self.build().execute(*args, **kwargs)
+            return CompoundSqlStatement(sql)
+        return SingleSqlStatement(sql)
 
     @staticmethod
     def _not_none(clause):
