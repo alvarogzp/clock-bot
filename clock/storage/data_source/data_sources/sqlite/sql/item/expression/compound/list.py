d88b12e05e0cb5eeecda77778c644a019e202d3e
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-27 12:04:58 +0100

    Improve ExpressionList parametrize the separator, and allow to add a before and after string

--- clock/storage/data_source/data_sources/sqlite/sql/item/expression/compound/list.py
+++ clock/storage/data_source/data_sources/sqlite/sql/item/expression/compound/list.py
@@ -2,13 +2,24 @@ from clock.storage.data_source.data_sources.sqlite.sql.item.expression.compound.
 from clock.storage.data_source.data_sources.sqlite.sql.item.expression.parser import EXPRESSION_TYPE
 
 
+DEFAULT_SEPARATOR = ", "
+DEFAULT_BEFORE = ""
+DEFAULT_AFTER = ""
+
+
 class ExpressionList(CompoundExpression):
-    def __init__(self, *expressions: EXPRESSION_TYPE):
+    def __init__(self, *expressions: EXPRESSION_TYPE, separator: str = DEFAULT_SEPARATOR,
+                 before: str = DEFAULT_BEFORE, after: str = DEFAULT_AFTER):
         self.expressions = [self.parse(expr) for expr in expressions]
+        self.separator = separator
+        self.before = before
+        self.after = after
 
     def str(self):
         expressions = (
             "{expr}".format(expr=expr.str())
             for expr in self.expressions
         )
-        return ", ".join(expressions)
+        expressions = self.separator.join(expressions)
+        return "{before}{expressions}{after}"\
+            .format(before=self.before, expressions=expressions, after=self.after)
