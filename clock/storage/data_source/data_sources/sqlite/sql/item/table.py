e1316976bb7e5abb6c92ccd30739fba6cd915379
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-12-01 19:03:58 +0100

    Create ColumnList inspired by ColumnListSchema

--- clock/storage/data_source/data_sources/sqlite/sql/item/table.py
+++ clock/storage/data_source/data_sources/sqlite/sql/item/table.py
@@ -1,5 +1,24 @@
+from collections import defaultdict
+
 from clock.storage.data_source.data_sources.sqlite.sql.item.base import NamedItem
+from clock.storage.data_source.data_sources.sqlite.sql.item.column import Column
 
 
 class Table(NamedItem):
     pass
+
+
+class ColumnList:
+    def __init__(self):
+        self._columns = []
+        self._versions = defaultdict(list)
+
+    def add(self, column: Column, version: int = 1):
+        self._columns.append(column)
+        self._versions[version].append(column)
+
+    def get_all(self):
+        return self._columns
+
+    def get_with_version(self, version: int):
+        return self._versions[version]
