490fe0ce8620a8c9cc98fe7fa2b40ff402f0783a
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-12-03 13:27:11 +0100

    Add OrClause to insert statement builder, to allow specifying alternative conflict resolution algorithms

--- clock/storage/data_source/data_sources/sqlite/sql/statement/builder/insert.py
+++ clock/storage/data_source/data_sources/sqlite/sql/statement/builder/insert.py
@@ -1,11 +1,12 @@
 from clock.storage.data_source.data_sources.sqlite.sql.item.expression.parser import EXPRESSION_TYPE, ExpressionParser
 from clock.storage.data_source.data_sources.sqlite.sql.statement.builder.base import StatementBuilder
 from clock.storage.data_source.data_sources.sqlite.sql.statement.builder.clauses.columns import ColumnsClause
+from clock.storage.data_source.data_sources.sqlite.sql.statement.builder.clauses.or_conflict import OrClause
 from clock.storage.data_source.data_sources.sqlite.sql.statement.builder.clauses.table import TableClause
 from clock.storage.data_source.data_sources.sqlite.sql.statement.builder.select import Select
 
 
-class Insert(TableClause, ColumnsClause, StatementBuilder):
+class Insert(OrClause, TableClause, ColumnsClause, StatementBuilder):
     def __init__(self):
         super().__init__()
         self._values = None
@@ -20,8 +21,11 @@ class Insert(TableClause, ColumnsClause, StatementBuilder):
         return self
 
     def build_sql(self):
+        sql = "insert"
+        if self._not_none(self._or):
+            sql += " {or_conflict_resolution}".format(or_conflict_resolution=self._or)
         columns = ", ".join(self._columns_names)
-        sql = "insert into {table} ({columns})".format(table=self._table, columns=columns)
+        sql += " into {table} ({columns})".format(table=self._table, columns=columns)
         if self._not_none(self._values):
             sql += " values ({values})".format(values=self._values)
         elif self._not_none(self._select):
