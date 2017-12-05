45e20a9ed847629da7be7281ae2bacdc96188e3e
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-12-05 23:44:26 +0100

    Implement ExpressionList on top of ParsedExpressionList

--- clock/storage/data_source/data_sources/sqlite/sql/item/expression/compound/list/generic.py
+++ clock/storage/data_source/data_sources/sqlite/sql/item/expression/compound/list/generic.py
@@ -1,25 +1,11 @@
 from clock.storage.data_source.data_sources.sqlite.sql.item.expression.compound.base import CompoundExpression
+from clock.storage.data_source.data_sources.sqlite.sql.item.expression.compound.list.parsed import ParsedExpressionList, \
+    DEFAULT_SEPARATOR, DEFAULT_BEFORE, DEFAULT_AFTER
 from clock.storage.data_source.data_sources.sqlite.sql.item.expression.parser import EXPRESSION_TYPE
 
 
-DEFAULT_SEPARATOR = ", "
-DEFAULT_BEFORE = ""
-DEFAULT_AFTER = ""
-
-
-class ExpressionList(CompoundExpression):
+class ExpressionList(ParsedExpressionList, CompoundExpression):
     def __init__(self, *expressions: EXPRESSION_TYPE, separator: str = DEFAULT_SEPARATOR,
                  before: str = DEFAULT_BEFORE, after: str = DEFAULT_AFTER):
-        self.expressions = [self.parse(expr) for expr in expressions]
-        self.separator = separator
-        self.before = before
-        self.after = after
-
-    def str(self):
-        expressions = (
-            "{expr}".format(expr=expr.str())
-            for expr in self.expressions
-        )
-        expressions = self.separator.join(expressions)
-        return "{before}{expressions}{after}"\
-            .format(before=self.before, expressions=expressions, after=self.after)
+        parsed_expressions = (self.parse(expr) for expr in expressions)
+        super().__init__(*parsed_expressions, separator=separator, before=before, after=after)
