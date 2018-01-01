9b15e4798bf55dba22b9ed9da0903ce7b4c591e3
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-01-01 23:59:06 +0100

    Update MatchSearchStrategyFactory to use SearchQuery

--- clock/finder/search_strategies/search_strategies/query/match/factory.py
+++ clock/finder/search_strategies/search_strategies/query/match/factory.py
@@ -1,3 +1,4 @@
+from clock.finder.query.query import SearchQuery
 from clock.finder.search_strategies.search_strategies.query.match.generic import GenericMatchSearchStrategy
 from clock.finder.zone_finder.zone_finders.localized import LocalizedZoneFinder
 from clock.finder.zone_finder.zone_finders.localized_date_time import LocalizedDateTimeZoneFinder
@@ -5,8 +6,8 @@ from clock.finder.zone_finder.zone_finders.name import NameZoneFinder
 
 
 class MatchSearchStrategyFactory:
-    def __init__(self, query_lower: str):
-        self.query_lower = query_lower
+    def __init__(self, query: SearchQuery):
+        self.query = query
 
     def zone_name(self, name_zone_finder: NameZoneFinder):
         return self._create(name_zone_finder.match_lower)
@@ -24,4 +25,4 @@ class MatchSearchStrategyFactory:
         return self._create(localized_date_time_zone_finder.match_tzname_lower)
 
     def _create(self, func: callable):
-        return GenericMatchSearchStrategy(self.query_lower, func)
+        return GenericMatchSearchStrategy(self.query.query_lower, func)
