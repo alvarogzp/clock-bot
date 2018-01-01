c5e47bed99716ab20f49ca5fd7c3b3ba9de2ced8
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-01-01 23:55:07 +0100

    Add copy() to SearchQuery

--- clock/finder/query/query.py
+++ clock/finder/query/query.py
@@ -1,4 +1,10 @@
+import copy
+
+
 class SearchQuery:
     def __init__(self, query_lower: str, lang: str = None):
         self.query_lower = query_lower
         self.lang = lang
+
+    def copy(self):
+        return copy.copy(self)
