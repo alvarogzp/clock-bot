311b24803cae7619a45fc0092ca3da3a4c1fe343
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-01-30 00:00:16 +0100

    Create SearchQueryWordParser and use it to parse search query words

--- clock/finder/query/parser.py
+++ clock/finder/query/parser.py
@@ -12,24 +12,15 @@ class SearchQueryParser:
     def parse(self):
         query_lower_words = []
         for word in self.query.split(" "):
-            is_param = self._check_params(word)
-            if not is_param:
-                query_lower_words.append(word.lower())
+            word = SearchQueryWordParser(word)
+            self._update_params(word)
+            if not word.is_param:
+                query_lower_words.append(word.lower)
         self.query_lower = " ".join(query_lower_words)
 
-    def _check_params(self, word: str):
-        param_found = False
-        lang = self._check_param(word, QUERY_PARAM_LANG)
-        if lang is not None:
-            param_found = True
-            self.param_lang = lang
-        return param_found
-
-    @staticmethod
-    def _check_param(word: str, param: str):
-        if word.lower().startswith(param):
-            return word[len(param):]
-        return None
+    def _update_params(self, word):
+        """:type word: SearchQueryWordParser"""
+        self.param_lang = word.get_param(QUERY_PARAM_LANG, self.param_lang)
 
     def parsed_query(self):
         return SearchQuery(self.query_lower, self.param_lang)
@@ -39,3 +30,26 @@ class SearchQueryParser:
         parser = SearchQueryParser(query)
         parser.parse()
         return parser.parsed_query()
+
+
+class SearchQueryWordParser:
+    def __init__(self, word: str):
+        self.word = word
+        self.word_lower = word.lower()
+        self.is_param = False
+
+    @property
+    def lower(self):
+        return self.word_lower
+
+    def get_param(self, param: str, default_value):
+        value = self._get_value(param)
+        if value is None:
+            return default_value
+        self.is_param = True
+        return value
+
+    def _get_value(self, param: str):
+        if self.word_lower.startswith(param):
+            return self.word[len(param):]
+        return None
