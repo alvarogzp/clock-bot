ed90ef33eff4fab6b3782a3eed6f75d402e75191
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-02-04 00:00:16 +0100

    Update SearchQuery to receive a dict of params

--- clock/finder/query/query.py
+++ clock/finder/query/query.py
@@ -1,10 +1,19 @@
 import copy
 
+from clock.finder.query.params import QUERY_PARAM_LANG
+
 
 class SearchQuery:
-    def __init__(self, query_lower: str, lang: str = None):
+    def __init__(self, query_lower: str, params: dict):
         self.query_lower = query_lower
-        self.lang = lang
+        self.params = params
+
+    @property
+    def lang(self):
+        return self.get_param(QUERY_PARAM_LANG)
+
+    def get_param(self, param: str):
+        return self.params.get(param)
 
     def is_empty(self):
         return not self.query_lower
