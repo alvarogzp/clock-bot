a8d69c82579be56d6c06e7c360db1cf6d958f4a2
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-02-08 00:00:16 +0100

    Create an AndSearchStrategyConcatenator

--- clock/finder/search_strategies/search_strategies/concatenator.py
+++ clock/finder/search_strategies/search_strategies/concatenator.py
@@ -1,4 +1,5 @@
 import itertools
+from collections import OrderedDict
 
 from clock.finder.search_strategies.strategy import SearchStrategy
 
@@ -18,3 +19,17 @@ class SearchStrategyConcatenator(SearchStrategy):
 class OrSearchStrategyConcatenator(SearchStrategyConcatenator):
     def get_results(self):
         return itertools.chain.from_iterable((strategy.get_results() for strategy in self.search_strategies))
+
+
+class AndSearchStrategyConcatenator(SearchStrategyConcatenator):
+    def get_results(self):
+        if len(self.search_strategies) == 0:
+            return ()
+        results = self.search_strategies[0].get_results()
+        results = list(OrderedDict.fromkeys(results))
+        for search_strategy in self.search_strategies[1:]:
+            search_strategy_results = search_strategy.get_results()
+            for result in results[:]:
+                if result not in search_strategy_results:
+                    results.remove(result)
+        return results
