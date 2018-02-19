170498a5a137660b30e431c3907788af29cafab2
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-02-20 00:00:16 +0100

    Fix advanced queries not working correctly

--- clock/finder/search_strategies/search_strategies/query/match/factory.py
+++ clock/finder/search_strategies/search_strategies/query/match/factory.py
@@ -1,3 +1,4 @@
+from clock.finder.query.params import QUERY_PARAM_TIME, QUERY_PARAM_GMT, QUERY_PARAM_TZNAME
 from clock.finder.query.query import SearchQuery
 from clock.finder.search_strategies.search_strategies.query.match.generic import GenericMatchSearchStrategy
 from clock.finder.zone_finder.zone_finders.localized import LocalizedZoneFinder
@@ -16,13 +17,17 @@ class MatchSearchStrategyFactory:
         return self._create(localized_zone_finder.match_names_lower)
 
     def time(self, localized_date_time_zone_finder: LocalizedDateTimeZoneFinder):
-        return self._create(localized_date_time_zone_finder.match_time_lower)
+        return self._create(localized_date_time_zone_finder.match_time_lower, QUERY_PARAM_TIME)
 
     def gmt_offset(self, localized_date_time_zone_finder: LocalizedDateTimeZoneFinder):
-        return self._create(localized_date_time_zone_finder.match_gmt_lower)
+        return self._create(localized_date_time_zone_finder.match_gmt_lower, QUERY_PARAM_GMT)
 
     def tzname(self, localized_date_time_zone_finder: LocalizedDateTimeZoneFinder):
-        return self._create(localized_date_time_zone_finder.match_tzname_lower)
+        return self._create(localized_date_time_zone_finder.match_tzname_lower, QUERY_PARAM_TZNAME)
 
-    def _create(self, func: callable):
-        return GenericMatchSearchStrategy(self.query.query_lower, func)
+    def _create(self, func: callable, param: str = None):
+        if param is None:
+            query = self.query.query_lower
+        else:
+            query = self.query.get_param(param).lower()
+        return GenericMatchSearchStrategy(query, func)
