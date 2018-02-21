80a769c8356cdc444ff801a34ba1976c182cb198
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-02-22 00:00:16 +0100

    Create SearchQueryParam

--- clock/finder/query/query.py
+++ clock/finder/query/query.py
@@ -3,6 +3,16 @@ import copy
 from clock.finder.query.params import QUERY_PARAM_LANG
 
 
+class SearchQueryParam:
+    def __init__(self, name: str, value: str):
+        self.name = name
+        self.value = value
+
+    @property
+    def value_lower(self):
+        return self.value.lower()
+
+
 class SearchQuery:
     def __init__(self, query_lower: str, params: dict):
         self.query_lower = query_lower
