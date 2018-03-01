d5884b76ecbc3ccab820825ee2cdb30fa2cb5cec
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-03-02 00:00:16 +0100

    Update MatchSearchStrategyFactory to receive directly the query string lowered, instead of a SearchQuery object, simplifying its logic

--- clock/finder/search_strategies/search_strategies/query/match/factory.py
+++ clock/finder/search_strategies/search_strategies/query/match/factory.py
@@ -1,5 +1,3 @@
-from clock.finder.query.params import QUERY_PARAM_TIME, QUERY_PARAM_GMT, QUERY_PARAM_TZNAME
-from clock.finder.query.query import SearchQuery
 from clock.finder.search_strategies.search_strategies.query.match.generic import GenericMatchSearchStrategy
 from clock.finder.zone_finder.zone_finders.localized import LocalizedZoneFinder
 from clock.finder.zone_finder.zone_finders.localized_date_time import LocalizedDateTimeZoneFinder
@@ -7,8 +5,8 @@ from clock.finder.zone_finder.zone_finders.name import NameZoneFinder
 
 
 class MatchSearchStrategyFactory:
-    def __init__(self, query: SearchQuery):
-        self.query = query
+    def __init__(self, query_lower: str):
+        self.query_lower = query_lower
 
     def zone_name(self, name_zone_finder: NameZoneFinder):
         return self._create(name_zone_finder.match_lower)
@@ -17,17 +15,13 @@ class MatchSearchStrategyFactory:
         return self._create(localized_zone_finder.match_names_lower)
 
     def time(self, localized_date_time_zone_finder: LocalizedDateTimeZoneFinder):
-        return self._create(localized_date_time_zone_finder.match_time_lower, QUERY_PARAM_TIME)
+        return self._create(localized_date_time_zone_finder.match_time_lower)
 
     def gmt_offset(self, localized_date_time_zone_finder: LocalizedDateTimeZoneFinder):
-        return self._create(localized_date_time_zone_finder.match_gmt_lower, QUERY_PARAM_GMT)
+        return self._create(localized_date_time_zone_finder.match_gmt_lower)
 
     def tzname(self, localized_date_time_zone_finder: LocalizedDateTimeZoneFinder):
-        return self._create(localized_date_time_zone_finder.match_tzname_lower, QUERY_PARAM_TZNAME)
+        return self._create(localized_date_time_zone_finder.match_tzname_lower)
 
-    def _create(self, func: callable, param: str = None):
-        if param is None:
-            query = self.query.query_lower
-        else:
-            query = self.query.get_param(param).lower()
-        return GenericMatchSearchStrategy(query, func)
+    def _create(self, func: callable):
+        return GenericMatchSearchStrategy(self.query_lower, func)
