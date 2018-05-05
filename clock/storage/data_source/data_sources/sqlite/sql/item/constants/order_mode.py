d01d04e01a22249401f2e14ec7488293b5309316
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-05-05 02:13:22 +0200

    Remove sql and component.migrate packages, and base component and version_info modules, as they belong now to sqlite-framework

--- clock/storage/data_source/data_sources/sqlite/sql/item/constants/order_mode.py
+++
@@ -1,9 +0,0 @@
-from clock.storage.data_source.data_sources.sqlite.sql.item.base import NamedItem
-
-
-class OrderMode(NamedItem):
-    pass
-
-
-ASC = OrderMode("asc")
-DESC = OrderMode("desc")
--- clock/storage/data_source/data_sources/sqlite/sql/result/row.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-from clock.storage.data_source.data_sources.sqlite.sql.util.column import ColumnUtil
-
-
-class ResultRow(sqlite3.Row):
-    def __getitem__(self, item):
-        item = ColumnUtil.name_if_column(item)
-        return super().__getitem__(item)
